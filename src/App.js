2dc63ef164550adf8d2dd439323b423430fea104
leduyson111 <sonldph11925@fpt.edu.vn>
2021-03-19 15:29:57 +0700

    add new project

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import Album from './components/Album';
+
+
+function App() {
+    const myName = "Lê DUY sƠN";
+    const myAge = 20;
+
+    const preson = {
+        name: "Lê Duy Sơn",
+        age: 20
+
+    }
+
+    return (
+
+    <div className = "App">
+    <div>
+        <Header></Header>
+        <main>
+            <Banner></Banner>
+            <Album></Album>
+        </main>
+        <Footer></Footer>
+    </div>
+
+
+        </div>
+
+    );
+ 
+}
+
+export default App;
